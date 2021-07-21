fe8446aeb4460ae51951c3b2e156c12e1c15acad
rekha381 <rekharam392@gmail.com>
2021-07-21 16:25:06 +0530

    Hiii test

---
+++ Jenkins/src/JenkinsPackage/Hi.java
@@ -0,0 +1,12 @@
+package JenkinsPackage;
+
+import org.omg.CORBA.RepositoryIdHelper;
+
+public class Hi {
+
+	public static void main(String[] args) {
+		System.out.println("this is Hi...");
+
+	}
+
+}
